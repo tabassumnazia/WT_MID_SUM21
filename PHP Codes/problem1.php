5d1555640e35407604d8f1c8f1a3591c6c67d5a8
tabassumnazia <tabassumnazia27@gmail.com>
2021-06-13 18:59:26 +0600

    (lab-3)late for electricity problem

---
+++ PHP Codes/problem1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Problem-1</title>
+</head>
+<body>
+"A quick brown
+<b>
+<?php
+$x = "fox";
+echo $x ;
+?>
+</b>
+jumps over the lazy
+<b>
+<?php
+$y = "dog";
+echo $y ;
+?>
+</b>
+"
+</body>
+</html>
---
+++ PHP Codes/problem3.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Problem 3</title>
+</head>
+<body>
+Perimeter =
+<?php
+    $length=20;
+    $width = 30;
+    $perimeter = 2*($length+$width);
+    print $perimeter;
+?>
+<br>Area =
+<?php
+    $length=20;
+    $width = 30;
+    $area = $length*$width;
+    print $area;
+?>
+</body>
+</html>
